7317ed82faa68a809ca19fe44f129ad651ad546f
David Garza <dgarza0413@gmail.com>
2020-03-05 19:09:58 -0600

    random card

--- src/pages/search.js
+++ src/pages/search.js
@@ -1,12 +1,52 @@
-import React from 'react'
-import baseUrl from '../utils/baseURL'
+import React, { useEffect, useState } from 'react';
+import baseUrl from '../utils/baseURL';
+import axios from 'axios';
 
-const search = () => {
+import PokemonCard from '../components/Card/PokemonCard';
+
+const Search = () => {
+    const [data, setData] = useState([])
+
+    const array = [...new Array(2)].map((e, i) => {
+        return e = Math.floor(Math.random() * 151) + 1
+    })
+
+    const randomSearch = () => {
+        try {
+            const arr = []
+            array.map((e, i) => {
+                axios.get(`https://pokeapi.co/api/v2/pokemon/${e}/`)
+                    .then(res => {
+                        arr.push(res.data)
+                        setData(arr)
+                    })
+            })
+        } catch (error) {
+            console.error(error)
+        }
+    }
+
+    useEffect(() => {
+        randomSearch()
+    }, [])
+
     return (
         <div>
-            This is the search page
+            {data.map((e, i) => {
+                return (
+                    <>
+                        {
+                            <PokemonCard
+                                name={e.name}
+                                types={e.types}
+                                sprite
+                            />
+                        }
+                    </>
+                )
+            })}
         </div>
     )
 }
 
-export default search
+export default Search
